2562a155316f762be0ed5942f99c9d190d7d51c8
Stuart Rimel <stuart_rimel@trimble.com>
2025-01-29 11:18:12 -0800

    Implement player activation and square selection logic in GameBoard and Player components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol className="highlight-player" id="players">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X" ? true : false}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O" ? true : false}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          activePlayer={activePlayer}
+          onSelectSquare={handleSelectSquare}
+        />
       </div>
     </main>
   );
