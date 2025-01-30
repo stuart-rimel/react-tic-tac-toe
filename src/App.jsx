4e576fedee1cca744c0d87bbfb006db57b05c5d5
Stuart Rimel <stuart_rimel@trimble.com>
2025-01-29 16:49:57 -0800

    Refactor game board and player management logic for improved state handling and clarity

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2",
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -25,47 +30,50 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-
-  const [playerNames, setPlayerNames] = useState({
-    X: "Player 1",
-    O: "Player 2",
-  });
+  const [playerNames, setPlayerNames] = useState(PLAYERS);
 
   const activePlayer = deriveActivePlayer(gameTurns);
-
-  // Overlooked bug, this needs to be a deep copy
-  // before it was assigned by reference to initialGameBoard
-  // and was mutating the initialGameBoard
-  let gameBoard = [...initialGameBoard.map((arrayRow) => [...arrayRow])];
-
-  // derived state from turns into gameBoard
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  let winner = null;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol =
-      gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =
-      gameBoard[combination[2].row][combination[2].column];
-
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = playerNames[firstSquareSymbol];
-    }
-  }
-
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, playerNames);
   const hasDraw = gameTurns.length === 9 && !winner;
+
+  function deriveGameBoard(gameTurns) {
+    // Overlooked bug, this needs to be a deep copy
+    // before it was assigned by reference to INITIAL_GAME_BOARD
+    // and was mutating the INITIAL_GAME_BOARD
+    let gameBoard = [...INITIAL_GAME_BOARD.map((arrayRow) => [...arrayRow])];
+
+    // derived state from turns into gameBoard
+    for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+    }
+    return gameBoard;
+  }
+
+  function deriveWinner(gameBoard, playerNames) {
+    let winner = null;
+
+    for (const combination of WINNING_COMBINATIONS) {
+      const firstSquareSymbol =
+        gameBoard[combination[0].row][combination[0].column];
+      const secondSquareSymbol =
+        gameBoard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol =
+        gameBoard[combination[2].row][combination[2].column];
+
+      if (
+        firstSquareSymbol &&
+        firstSquareSymbol === secondSquareSymbol &&
+        firstSquareSymbol === thirdSquareSymbol
+      ) {
+        winner = playerNames[firstSquareSymbol];
+      }
+    }
+    return winner;
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -98,13 +106,13 @@ function App() {
       <div id="game-container">
         <ol className="highlight-player" id="players">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === "X" ? true : false}
             onChangeName={handlePlayerNameChange}
           />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === "O" ? true : false}
             onChangeName={handlePlayerNameChange}
